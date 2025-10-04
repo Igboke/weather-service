e9387002bf8e6a3120c47e51a486a934f3f2e3c1
igboke <danieligboke669@gmail.com>
2025-10-04 14:02:06 +0100

    feat: added persistence

---
+++ src/main/java/com/example/weather_service/repository/CityRepository.java
@@ -0,0 +1,14 @@
+package com.example.weather_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.weather_service.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long>{
+
+    City findByCityName(String cityName);
+
+    
+}
