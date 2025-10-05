3c2074bebba366e7c9e0d100aa913f952d6fbfb1
igboke <danieligboke669@gmail.com>
2025-10-05 15:53:04 +0100

    feat: completed the current weathe GET request

--- src/main/java/com/example/weather_service/service/WeatherServiceImplementation.java
+++ src/main/java/com/example/weather_service/service/WeatherServiceImplementation.java
@@ -13,6 +13,11 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @Cacheable("weather")
 public class WeatherServiceImplementation implements WeatherService{
@@ -21,6 +26,9 @@ public class WeatherServiceImplementation implements WeatherService{
     private final String apiUrl;
     private final String apiKey;
     private final CityRepository cityRepository;
+    String formattedTimestamp = null;
+    String sunrise = null;
+    String sunset = null;
 
     public WeatherServiceImplementation (RestTemplate restTemplate,
                                         CityRepository cityRepository,
@@ -44,13 +52,25 @@ public class WeatherServiceImplementation implements WeatherService{
         try{
             OpenWeatherMapResponse response = restTemplate.getForObject(url, OpenWeatherMapResponse.class);
 
-            if (response != null && response.getMain() != null) {
+            if (response != null && response.getMain() != null && response.getSys() != null) {
+                String description = null;
+                String conditions = null;
+                if (response.getWeather() != null && !response.getWeather().isEmpty()) {
+                    description = response.getWeather().get(0).getDescription();
+                    conditions = response.getWeather().get(0).getConditions();
+                    }
+                formattedTimestamp = formatTimestamp(response,response.getDt());
+                sunrise = formatTimestamp(response, response.getSys().getSunrise());
+                sunset = formatTimestamp(response,response.getSys().getSunset());
                 try {
-                cityRepository.save(new City(response.getCity()));
-            } catch (Exception e) {
-                System.err.println("Could not save city: " + e.getMessage());
-            }
-                return new CurrentWeatherResponse(response.getCity(), response.getMain().getTemperature());
+                    cityRepository.save(new City(response.getCity()));
+                } catch (Exception e) {
+                    System.err.println("Could not save city: " + e.getMessage());
+                }
+                return new CurrentWeatherResponse(response.getCity(), response.getMain().getTemperature(),
+                response.getSys().getCountry(),formattedTimestamp,sunrise,sunset,response.getMain().getHumidity(),
+                response.getMain().getPressure(),response.getWind().getWindSpeed(),response.getWind().getWindDirection(),
+                conditions,description);
             } else{
                 throw new CityNotFoundException("Weather data not available for city: " + city);
             }
@@ -61,6 +81,21 @@ public class WeatherServiceImplementation implements WeatherService{
         throw new RuntimeException("API Client Error: " + e.getStatusCode());
     }   
         
+    }
+
+    private String formatTimestamp(OpenWeatherMapResponse response, long dateTime){
+        try{
+            Instant instant = Instant.ofEpochSecond(dateTime);
+            ZoneOffset zoneOffset = ZoneOffset.ofTotalSeconds(response.getTimezone());
+            LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneOffset);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            formattedTimestamp = localDateTime.format(formatter);
+            return formattedTimestamp;
+
+        }catch (Exception e){
+            System.err.println("Could not convert time object" + e.getMessage());
+            return null;
+        }
     }
     
 }
