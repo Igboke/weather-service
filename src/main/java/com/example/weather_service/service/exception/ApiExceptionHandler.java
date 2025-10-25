a4147bdbc294868bb84258e60342a9fd09d9de0e
igboke <danieligboke669@gmail.com>
2025-10-25 20:49:32 +0100

    fix returned the essential api exception handler

--- src/main/java/com/example/weather_service/service/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/weather_service/service/exception/ApiExceptionHandler.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    // @ExceptionHandler(CityNotFoundException.class)
-    // public ResponseEntity<ApiErrorResponse> handleCityNotFoundException(CityNotFoundException ex) {
-    //     ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
-    //     return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    // }
+    @ExceptionHandler(CityNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleCityNotFoundException(CityNotFoundException ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
-    // @ExceptionHandler(Exception.class)
-    // public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex) {
-    //     ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred");
-    //     return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred",null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     
 }
